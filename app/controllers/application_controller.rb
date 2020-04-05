4eacfc01c12060b83d07f7b254b81de503f068af
AntonCherkago <antonkonovalov167@gmail.com>
2020-04-06 00:16:16 +0300

    last fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
-
+  
   def send_message
     token = ENV['TELEGRAM_BOT_TOKEN']
     channel = ENV['TELEGRAM_BOT_CHANNEL']
