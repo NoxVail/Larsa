47a462a9a2dd60fb6ce627210537d3945c837bf0
AntonCherkago <antonkonovalov167@gmail.com>
2020-04-15 22:12:51 +0300

    delete yarn, ngrock & other unused gems

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,13 @@
 require_relative 'boot'
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+config.bot_token = ENV.fetch(‘BOT_TOKEN’)
+config.bot_сhannel = ENV.fetch(‘BOT_CHANNEL’)
 
 module Larsa
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
