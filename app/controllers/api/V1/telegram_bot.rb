28e36ab4327ec25ab41b8db29cf14208ea98e001
AntonCherkago <antonkonovalov167@gmail.com>
2020-04-21 23:08:21 +0300

    bot_params_validation & some other fixes after pull request

--- app/controllers/api/V1/telegram_bot.rb
+++ app/controllers/api/V1/telegram_bot.rb
@@ -3,8 +3,8 @@ module API
     class TelegramBot < Grape::API
       desc 'telegram_bot'
       post root: :message do
-        token = ENV['TELEGRAM_BOT_TOKEN']
-        channel = ENV['TELEGRAM_BOT_CHANNEL']
+        token = Rails.configuration.bot_token
+        channel = Rails.configuration.bot_channel
         text = params[:message][:text].to_s.gsub('/', '')
         bot = Telegram::Bot::Client.new(token)
         bot.send_message(chat_id: channel, text: text)
